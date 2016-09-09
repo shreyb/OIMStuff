8395bab3975a6ac94c7a69b732e484e622965f03
sbhat <sbhat@fnal.gov>
2016-09-09 10:50:27 -0500

    Changed get_data_from_OIM to use requests module

--- OIM_Stuff/get_data_from_OIM.py
+++ OIM_Stuff/get_data_from_OIM.py
@@ -1,14 +1,18 @@
-import subprocess
+import requests
 
 # Note:  URL needs to take into account current date!
 
+requests_test_file = 'resource_group_TEST.xml'
+OIM_url = 'http://myosg.grid.iu.edu/rgsummary/xml?summary_attrs_showhierarchy=on&summary_attrs_showwlcg=on&summary_attrs_showservice=on&summary_attrs_showfqdn=on&summary_attrs_showvoownership=on&summary_attrs_showcontact=on&gip_status_attrs_showtestresults=on&downtime_attrs_showpast=&account_type=cumulative_hours&ce_account_type=gip_vo&se_account_type=vo_transfer_volume&bdiitree_type=total_jobs&bdii_object=service&bdii_server=is-osg&start_type=7daysago&start_date=08%2F31%2F2016&end_type=now&end_date=08%2F31%2F2016&all_resources=on&facility_sel%5B%5D=10009&gridtype=on&gridtype_1=on&active=on&active_value=1&disable_value=1'
 
-try:
-    subprocess.check_call(['wget',
-                       'http://myosg.grid.iu.edu/rgsummary/xml?summary_attrs_showhierarchy=on&summary_attrs_showwlcg=on&summary_attrs_showservice=on&summary_attrs_showfqdn=on&summary_attrs_showvoownership=on&summary_attrs_showcontact=on&gip_status_attrs_showtestresults=on&downtime_attrs_showpast=&account_type=cumulative_hours&ce_account_type=gip_vo&se_account_type=vo_transfer_volume&bdiitree_type=total_jobs&bdii_object=service&bdii_server=is-osg&start_type=7daysago&start_date=08%2F31%2F2016&end_type=now&end_date=08%2F31%2F2016&all_resources=on&facility_sel%5B%5D=10009&gridtype=on&gridtype_1=on&active=on&active_value=1&disable_value=1',
-                       '-O', 'resource_group_TEST.xml'])
-except:
-    print "NOPE!"
+r = requests.get(OIM_url)
 
+if r.status_code == requests.codes.ok:
+    with open(requests_test_file, 'w') as f:
+        f.write(r.content)
+else:
+    print "Couldn't get OIM file"
+
+# print r.content
 
 
